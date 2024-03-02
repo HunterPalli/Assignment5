5e77ab8f1a23dffd3bbba1702541dd2240eb788d
HunterPalli <158235663+HunterPalli@users.noreply.github.com>
2024-03-02 00:23:46 -0500

    Update binarySearchTree.java

--- accidentpack/binarySearchTree.java
+++ accidentpack/binarySearchTree.java
@@ -1,4 +1,33 @@
 
-public class binarySearchTree {
+public class binarySearchTree<E> {
+	private static class Node<E> {
+        E data;
+        Node<E> left, right;
 
+        public Node(E object) {
+            data = object;
+            left = right = null;
+        }
+    }
+    Node<E> root;
+
+    public binarySearchTree() {
+        root = null;
+    }
+    
+    public void add(int value) {
+        root = insert(root, value);
+    }
+
+    private Node<E> insert(Node<E> root, E object) {
+        if (root == null) {
+            root = new Node<E>(object);
+            return root;
+        }
+
+        //Need binary search tree sorted insert if statements
+
+        return root;
+    }
+	
 }
