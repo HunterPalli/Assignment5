7dea36f9a503a7eb4e79493df0c5109f2cd6ecae
HunterPalli <158235663+HunterPalli@users.noreply.github.com>
2024-03-04 11:22:36 -0500

    Update program5.java
    now uses state to return a binarySearchTree for specific state

--- accidentpack/program5.java
+++ accidentpack/program5.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class program5 {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		binarySearchTree<Report> test = Scan("accidentpack/accidents_small_sample.csv");
+		binarySearchTree<Report> test = Scan("accidentpack/accidents_small_sample.csv", "CA");
 		test.printTreeInOrder();
 	}
 	
-	public static binarySearchTree<Report> Scan(String loc) throws FileNotFoundException {
+	public static binarySearchTree<Report> Scan(String loc, String state) throws FileNotFoundException {
 		binarySearchTree<Report> fin = new binarySearchTree<Report>();
 		long start = System.currentTimeMillis();
 		Scanner scnr = new Scanner(new File(loc));
@@ -16,7 +16,9 @@ public class program5 {
 		while(scnr.hasNextLine()) {
 			String split[] = scnr.nextLine().split(",");
 			Report reported = new Report(split[0], (int) Integer.parseInt(split[1]), split[2], split[3], split[4], split[5], split[6], split[7], (int) Double.parseDouble(split[8]), (int) Double.parseDouble(split[9]), (int) Double.parseDouble(split[10]), split[11], Boolean.parseBoolean(split[12]), split[13]);
-			fin.add(reported);
+			if(reported.getState().equals(state)) {
+				fin.add(reported);
+			}
 		}
 		long end = System.currentTimeMillis();
 		System.out.println((end-start) + " milliseconds to read the records into a Binary Search Tree");
