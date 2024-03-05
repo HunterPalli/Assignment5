e3ed5df1a8d8a21f6b1149e165e86ecb8328ec9c
HunterPalli <158235663+HunterPalli@users.noreply.github.com>
2024-03-04 20:25:16 -0500

    Comments + Final touches + PDF

--- accidentpack/binarySearchTree.java
+++ accidentpack/binarySearchTree.java
@@ -3,6 +3,8 @@ public class binarySearchTree<E extends Comparable<Report>> {
 	private static class Node<E> {
         E data;
         Node<E> left, right;
+        int leftChldrn = 0;
+        int rightChldrn = 0;
 
         public Node(E object) {
             data = object;
@@ -11,41 +13,72 @@ public class binarySearchTree<E extends Comparable<Report>> {
     }
     Node<E> root;
 
+    /**
+     * base constructor for creating empty BST
+     */
     public binarySearchTree() {
         root = null;
     }
     
+    /**
+     * Add method for adding items to the BST
+     * @param value
+     */
     public void add(E value) {
         root = insert(root, value);
     }
 
-    private Node<E> insert(Node<E> root, E object) {
-        if (root == null) {
-            root = new Node<E>(object);
-            return root;
+    /**
+     * Helper method for inserting reports in the correct place
+     * to adhere to BST standards
+     * @param start
+     * @param object
+     * @return
+     */
+    private Node<E> insert(Node<E> start, E object) {
+        if (start == null) {
+            start = new Node<E>(object);
+            return start;
         }
-        if (compare(object, root.data) < 0) {
-			root.left = insert(root.left, object);
-		} else if (compare(object, root.data) == 0) {
-			root.right = insert(root.right, object);
-		} else if (compare(object, root.data) > 0) {
-			root.right = insert(root.right, object);
+        if (compare(object, start.data) < 0) {
+			start.left = insert(start.left, object);
+			start.leftChldrn += 1;
+		} else if (compare(object, start.data) == 0) {
+			start.right = insert(start.right, object);
+			start.rightChldrn += 1;
+		} else if (compare(object, start.data) > 0) {
+			start.right = insert(start.right, object);
+			start.rightChldrn += 1;
 		}
 
       
 
-        return root;
+        return start;
     }
     
+    /**
+     * Compare helper method for comparing the start times of reports in the
+     * BST to dates in string form
+     * @param object
+     * @param a
+     * @return value 
+     */
     private int compare(E object, E a) {
     	
     	return object.compareTo((Report) a);
     }
 	
+    /**
+     * public print method for printing BST in order
+     */
     public void printTreeInOrder() {
     	inOrderTree(root);
     }
-    private void inOrderTree(Node nd) {
+    /**
+     * Helper method for printing the BST in order
+     * @param nd
+     */
+    private void inOrderTree(Node<E> nd) {
     	if(nd == null) {
     		return;
     	}
@@ -85,5 +118,41 @@ public class binarySearchTree<E extends Comparable<Report>> {
         count += getTotalHelper(node.left);
         count += getTotalHelper(node.right);
         return count;
+    }
+    
+    /**
+     * Method to return total nodes in BST
+     * @return
+     */
+    public int getChildren() {
+    	return root.leftChldrn + root.rightChldrn + 1;
+    }
+    
+    /**
+     * Method to find the amount of reports
+     * that happened on/after a given time without recursion.
+     * @param time
+     * @return count of child nodes under the first node that has start time within given time
+     */
+    public int findChildren(String time) {
+    	Node<E> n = root;
+    	Report e = (Report) n.data;
+    	if(e.getStart_Time().compareTo(time) > 0) {
+    		Report f = (Report) n.left.data;
+    		int chk = 0;
+    		while(f.getStart_Time().compareTo(time) >= 0) {
+    			chk++;
+    			chk += n.rightChldrn;
+    			n = n.left;
+    			f = (Report) n.left.data;
+    		} return n.rightChldrn + chk + 1;
+    	} n = root;
+    	while(e.getStart_Time().compareTo(time) < 0) {
+    		n = n.right;
+    		if(n.data == null) {
+    			return 0;
+    		}
+        	e = (Report) n.data;
+    	} return n.rightChldrn + 1;
     }
 }
--- accidentpack/program5.java
+++ accidentpack/program5.java
@@ -10,11 +10,29 @@ public class program5 {
 		String state = scn.next(); //format: CA
 		System.out.println("Please enter the time to search by: ");
 		String time = scn.next(); //format: YYYY-MM-DD
-		binarySearchTree<Report> test = Scan("accidentpack/accidents_small_sample.csv", state);
-		//Need something to calculate number of reports on and after the time given by the user.
-		test.printTreeInOrder();
+		binarySearchTree<Report> test = Scan(args[0], state);
+		
+		long start = System.currentTimeMillis();
+		int abc2 = test.findChildren(time);
+		long end = System.currentTimeMillis();
+		System.out.println((end-start) + " milliseconds to count the records after the provided time using children count fields.");
+		System.out.println(abc2);
+		
+		start = System.currentTimeMillis();
+		int abc = test.getCountTime(time);
+		end = System.currentTimeMillis();
+		System.out.println((end-start) + " milliseconds to count the records after the provided time using recursion.");
+		System.out.println(abc);
 	}
 	
+	/**
+	 * Scan method for reading CSV files line by line and adding them to the
+	 * BinarySearchTree
+	 * @param loc
+	 * @param state
+	 * @return
+	 * @throws FileNotFoundException
+	 */
 	public static binarySearchTree<Report> Scan(String loc, String state) throws FileNotFoundException {
 		binarySearchTree<Report> fin = new binarySearchTree<Report>();
 		long start = System.currentTimeMillis();
